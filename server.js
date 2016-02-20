74c2b7c7221ae26dfb0fb75edcf3788d202168ac
Danny <djpeck@iupui.edu>
2016-02-20 06:17:00 -0500

    added content-type origin fix

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ var signup = require('./routes/signup');
 
 var server = express();
 
+
+server.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 // view engine set up
 server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'jade');
